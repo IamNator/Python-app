06f75191370c2cae7a7f897604e9e0cbbdbe54a5
IamNator <natverior1@gmail.com>
2021-04-16 16:30:36 +0100

    commenting

--- api.py
+++ api.py
@@ -3,7 +3,7 @@ from flask import Flask           # import flask
 app = Flask(__name__)             # create an app instance
 
 @app.route("/<name>")                   # at the end point /
-def hello_name(name):                      # call method hello
+def hello_name(name):                   # call method hello_name
     return "Hello "+name         # which returns "hello world"
 if __name__ == "__main__":        # on running python app.py
     app.run()                     # run the flask app
