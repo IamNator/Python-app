14e39d84a339e12851ec6bdd3d80f4b25885b08c
IamNator <natverior1@gmail.com>
2021-04-16 16:49:36 +0100

    added a another Get method for catalog

--- api.py
+++ api.py
@@ -1,9 +1,36 @@
 # app.py
 from flask import Flask           # import flask
+from flask import jsonify
 app = Flask(__name__)             # create an app instance
+app.config["DEBUG"] = True
 
-@app.route("/<name>")                   # at the end point /
+#Create some test data for our catalogue in the form of a list of dictionaries.
+books = [
+    {'id': 0,
+     'title': 'A Fire Upon the Deep',
+     'author': 'Vernor Vinge',
+     'first_sentence': 'The coldsleep itself was dreamless.',
+     'year_published': '1992'},
+    {'id': 1,
+     'title': 'The Ones Who Walk Away From Omelas',
+     'author': 'Ursula K. Le Guin',
+     'first_sentence': 'With a clamor of bells that set the swallows soaring, the Festival of Summer came to the city Omelas, bright-towered by the sea.',
+     'published': '1973'},
+    {'id': 2,
+     'title': 'Dhalgren',
+     'author': 'Samuel R. Delany',
+     'first_sentence': 'to wound the autumnal city.',
+     'published': '1975'}
+] 
+
+@app.route("/<name>", methods=['GET'])                   # at the end point /
 def hello_name(name):                   # call method hello_name
     return "Hello "+name         # which returns "hello world"
-if __name__ == "__main__":        # on running python app.py
-    app.run()                     # run the flask app
+
+# A route to return  all the available entried in the catalog.
+@app.route("/api/v1/resources/books/all", methods=['GET'])
+def api_all():
+    return jsonify(books)
+
+app.run()
+
